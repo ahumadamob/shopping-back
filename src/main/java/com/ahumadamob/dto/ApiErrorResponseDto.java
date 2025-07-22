75f6265b9dcf447cf6f95dbc6af77f8ab6ea6374
Mario Ahumada <ahumadamob@gmail.com>
2025-07-22 13:49:23 -0300

    Rename ApiErrorResponse to ApiErrorResponseDto

--- src/main/java/com/ahumadamob/dto/ApiErrorResponseDto.java
+++ src/main/java/com/ahumadamob/dto/ApiErrorResponseDto.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ApiErrorResponse {
+public class ApiErrorResponseDto {
 
     private String message;
     private LocalDateTime timestamp;
