109e0e2124b6ef87f6316c12447e2e23a95fbd17
Mario Ahumada <ahumadamob@gmail.com>
2025-07-21 23:29:15 -0300

    Add BaseEntity and extend Categoria

--- src/main/java/com/ahumadamob/entity/Categoria.java
+++ src/main/java/com/ahumadamob/entity/Categoria.java
@@ -2,10 +2,8 @@ package com.ahumadamob.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import com.ahumadamob.entity.BaseEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,11 +15,7 @@ import jakarta.validation.constraints.Size;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Categoria {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Categoria extends BaseEntity {
 
     @Column(name = "nombre", nullable = false, length = 64)
     @NotBlank
